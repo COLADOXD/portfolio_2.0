9c73203dac13f1161169a52e0993c3b7f3b9a5bd
Sebas Trelles <xdmateo90@gmail.com>
2024-11-29 17:45:43 -0500

    Change Name of file and title

---
+++ src/components/WorkExperienceComponent.tsx
@@ -0,0 +1,11 @@
+import WorkExperienceIcon from "../assets/svg/WorkExperience";
+export const WorkExperienceComponent = () => {
+  return (
+    <div className="px-4 flex items-start justify-start">
+      <div className="flex ">
+        <WorkExperienceIcon className="size-8 dark:text-white mr-4" />
+        <h1>Work Experience</h1>
+      </div>
+    </div>
+  );
+};
