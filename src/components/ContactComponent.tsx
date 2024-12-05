003954d4e949c790f01a2e184fdeff0e3b837fa1
Sebas Trelles <xdmateo90@gmail.com>
2024-12-05 13:19:15 -0500

    add flex wrap

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -11,19 +11,19 @@ const ListContacts = [
 
 export const ContactComponent = () => {
   return (
-    <div className="flex flex-col mb-10 w-screen max-w-[800px] ml-4 items-start justify-start">
+    <div className="flex flex-col mb-10 w-screen max-w-[800px] pl-8 items-start justify-start">
       <div className="flex items-center justify-center">
         <ContactIcon className="size-12 dark:text-white mr-4" />
         <h1>Contact</h1>
       </div>
-      <div className="flex mt-8">
+      <div className="flex mt-8 flex-wrap">
         {ListContacts.map((contact, key) => {
           const IconComponent = contact.icon;
           return (
             <a
               href={contact.link}
               target="_blank"
-              className="flex items-center p-4 mr-4 rounded-3xl border-2 border-gray-200 dark:border-gray-900"
+              className="flex items-center mb-2 p-4 mr-4 rounded-3xl border-2 border-gray-200 dark:border-gray-900"
               key={key}
             >
               <IconComponent className="size-12 dark:text-white" />
